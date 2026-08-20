46494e5bde7d88c95558a59cdb4de5941813362e
gl4a-dev <gl4aveloso.dev@gmail.com>
2026-08-20 09:11:16 -0300

    test: include new flags in PromptBuilder tests

--- tests/builders/test_prompt_builder.py
+++ tests/builders/test_prompt_builder.py
@@ -1,10 +1,32 @@
 from pathlib import Path
+import pytest
 
 from promptforge.builders.prompt_builder import PromptBuilder
 from promptforge.scanner.scanner import Scanner
 from promptforge.filters.gitignore_filter import GitIgnoreFilter
 from promptforge.filters.pipline import FilterPipeline
 
+
+@pytest.mark.parametrize(
+    "build_tree, build_content",
+    [
+        (True, True),
+        (True, False),
+        (False, True),
+    ],
+)
+def test_build_prompt_valid_initialization(build_tree: bool, build_content: bool) -> None:
+    PromptBuilder(
+        build_tree=build_tree,
+        build_content=build_content,
+    )
+
+def test_build_prompt_invalid_initialization() -> None:
+    with pytest.raises(ValueError):
+        PromptBuilder(
+            build_tree=False,
+            build_content=False,
+        )
 
 def test_build_prompt(tmp_path: Path) -> None:
     src = tmp_path / "src"
@@ -36,6 +58,82 @@ def test_build_prompt(tmp_path: Path) -> None:
         "└── README.md",
         "```",
         "",
+        "# File Contents",
+        "",
+        "## src/main.py",
+        "",
+        "```python",
+        'print("Hello")',
+        "",
+        "```",
+        "",
+        "## README.md",
+        "",
+        "```markdown",
+        "# Example",
+        "",
+        "```",
+        "",
+    ])
+
+def test_build_only_tree_prompt(tmp_path: Path) -> None:
+    src = tmp_path / "src"
+    src.mkdir()
+
+    main = src / "main.py"
+    main.write_text(
+        'print("Hello")\n',
+        encoding="utf-8",
+    )
+
+    readme = tmp_path / "README.md"
+    readme.write_text(
+        "# Example\n",
+        encoding="utf-8",
+    )
+
+    scan_result = Scanner().scan(tmp_path)
+
+    result = PromptBuilder(
+        build_tree=True, 
+        build_content=False
+    ).build(scan_result)
+
+    assert result == "\n".join([
+        "# Project Structure",
+        "",
+        "```text",
+        tmp_path.name,
+        "├── src",
+        "│   └── main.py",
+        "└── README.md",
+        "```",
+    ])
+
+def test_build_only_content_prompt(tmp_path: Path) -> None:
+    src = tmp_path / "src"
+    src.mkdir()
+
+    main = src / "main.py"
+    main.write_text(
+        'print("Hello")\n',
+        encoding="utf-8",
+    )
+
+    readme = tmp_path / "README.md"
+    readme.write_text(
+        "# Example\n",
+        encoding="utf-8",
+    )
+
+    scan_result = Scanner().scan(tmp_path)
+
+    result = PromptBuilder(
+        build_tree=False, 
+        build_content=True
+    ).build(scan_result)
+
+    assert result == "\n".join([
         "# File Contents",
         "",
         "## src/main.py",
